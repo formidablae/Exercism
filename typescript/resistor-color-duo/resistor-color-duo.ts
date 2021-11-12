f77357761a22a3496e5b13bbecfbfb528969ab2d
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-12 07:46:03 +0100

    Solving TypeScript exercises

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,52 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(...bandColors: string[]) {
+  let answer: string = "";
+  let i: number = 0;
+  while (i < 2) {
+    console.log(bandColors[0][i]);
+    switch(bandColors[0][i]) { 
+      case "black": { 
+        answer += "0";
+        break; 
+      }
+      case "brown": { 
+        answer += "1";
+        break; 
+      }
+      case "red": { 
+        answer += "2";
+        break; 
+      }
+      case "orange": { 
+        answer += "3";
+        break; 
+      }
+      case "yellow": { 
+        answer += "4";
+        break; 
+      }
+      case "green": { 
+        answer += "5";
+        break; 
+      }
+      case "blue": { 
+        answer += "6";
+        break; 
+      }
+      case "violet": { 
+        answer += "7";
+        break; 
+      }
+      case "grey": { 
+        answer += "8";
+        break; 
+      }
+      case "white": { 
+        answer += "9";
+        break; 
+      }
+      default: { break; }
+    }
+    i++;
+  }
+  return Number(answer);
 }
