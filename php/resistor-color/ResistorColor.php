5af50e5002358f4314724b067a2a0615854ad0d3
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-22 01:19:53 +0100

    Solved resistor-color in php exercises

--- php/resistor-color/ResistorColor.php
+++ php/resistor-color/ResistorColor.php
@@ -24,7 +24,20 @@
 
 declare(strict_types=1);
 
+const COLORS = [
+    'black' => 0,
+    'brown' => 1,
+    'red' => 2,
+    'orange' => 3,
+    'yellow' => 4,
+    'green' => 5,
+    'blue' => 6,
+    'violet' => 7,
+    'grey' => 8,
+    'white' => 9,
+];
+
 function colorCode(string $color): int
 {
-    throw new \BadFunctionCallException("Implement the colorCode function");
+    return self::COLORS[$color];
 }
