418e3cef03a21dff8f25c807de2824fb2de49a4c
formidablae <81068781+formidablae@users.noreply.github.com>
2021-12-08 22:34:44 +0100

    Solved Markdown Python exercise

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -2,76 +2,21 @@ import re
 
 
 def parse(markdown):
-    lines = markdown.split('\n')
-    res = ''
-    in_list = False
-    in_list_append = False
-    for i in lines:
-        if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
-        elif re.match('##### (.*)', i) is not None:
-            i = '<h5>' + i[6:] + '</h5>'
-        elif re.match('#### (.*)', i) is not None:
-            i = '<h4>' + i[5:] + '</h4>'
-        elif re.match('### (.*)', i) is not None:
-            i = '<h3>' + i[4:] + '</h3>'
-        elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
-        elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
-        m = re.match(r'\* (.*)', i)
-        if m:
-            if not in_list:
-                in_list = True
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                    is_italic = True
-                i = '<ul><li>' + curr + '</li>'
-            else:
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                i = '<li>' + curr + '</li>'
-        else:
-            if in_list:
-                in_list_append = True
-                in_list = False
-
-        m = re.match('<h|<ul|<p|<li', i)
-        if not m:
-            i = '<p>' + i + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
-        if in_list_append:
-            i = '</ul>' + i
-            in_list_append = False
-        res += i
-    if in_list:
-        res += '</ul>'
-    return res
+    res = []
+    for line in markdown.split('\n'):                   # split by line
+        line = re.sub(r'__(.*?)__', r'<strong>\1</strong>', line)
+        line = re.sub(r'_(.*?)_', r'<em>\1</em>', line)
+        header_match = re.match(r'(#+) (.*)', line)     # match #### header
+        if header_match and len(header_match.group(1)) < 7:
+            res.append('<h{0}>{1}</h{0}>'.format(len(header_match.group(1)), header_match.group(2)))
+        elif line.startswith('* '):                     # list
+            if res and res[-1] == '</ul>':              # if previous line was a list
+                res.pop()                               # remove the last </ul>
+            else:                                       # if previous line was not a list
+                res.append('<ul>')                      # start a new list
+            res.append('<li>' + line[2:] + '</li>')     # add list item
+            res.append('</ul>')                         # end list
+        else:                                           # if not header or list, but paragraph
+            res.append('<p>' + line + '</p>')           # add paragraph
+    return ''.join(res)                                 # join all lines into a string
+ 
