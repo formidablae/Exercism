e55d4e9ebb445ade85f6d99c0113cc4ab55086e6
formidablae <81068781+formidablae@users.noreply.github.com>
2023-02-13 00:47:52 +0100

    Solved Two Fer Java exercise

---
+++ java/two-fer/src/test/java/TwoferTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TwoferTest {
+
+    private Twofer twofer;
+
+    @Before
+    public void setup() {
+        twofer = new Twofer();
+    }
+
+    @Test
+    public void noNameGiven() {
+        assertThat(twofer.twofer(null))
+                .isEqualTo("One for you, one for me.");
+    }
+
+    @Test
+    public void aNameGiven() {
+        assertThat(twofer.twofer("Alice"))
+                .isEqualTo("One for Alice, one for me.");
+    }
+
+    @Test
+    public void anotherNameGiven() {
+        assertThat(twofer.twofer("Bob"))
+                .isEqualTo("One for Bob, one for me.");
+    }
+
+}
